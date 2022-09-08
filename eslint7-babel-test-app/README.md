722c18fdc0fc2ea3c092d5869d863b1a9cde172d
Victor Lin <13424970+victorlin@users.noreply.github.com>
2022-09-08 11:09:12 -0700

    eslint7 with babel works fine

---
+++ eslint7-babel-test-app/README.md
@@ -0,0 +1,18 @@
+# eslint7-babel-test-app
+
+Test app to show ESLint error:
+
+```
+Parsing error: Cannot use keyword 'await' outside an async function
+```
+
+## Setup commands
+
+```sh
+# create app.js
+npm init --yes
+npm install --save-dev eslint@7
+npm install --save-dev @babel/eslint-parser
+npm install --save-dev @babel/core
+npx eslint --init
+```
---
+++ eslint7-babel-test-app/app.js
@@ -0,0 +1,17 @@
+/* https://nodejs.org/en/docs/guides/getting-started-guide/ */
+
+// import http from 'http';
+const http = await import('http');
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World');
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
